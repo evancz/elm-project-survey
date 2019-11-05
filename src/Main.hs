c147d689763792d0df33702b56d9b94930884ad9
Evan Czaplicki <evancz@users.noreply.github.com>
2019-11-05 15:21:39 -0500

    add logic for estimating asset sizes

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
 module Main (main) where
 
 
@@ -15,13 +15,17 @@ import qualified Data.Time.Clock as Time
 import qualified Data.Time.Clock.System as Time
 import Data.Word (Word16)
 import qualified Details
+import Control.Applicative ((<|>))
+import qualified Control.Exception as E
 import qualified Control.Monad as M
 import qualified System.Directory as Dir
 import qualified System.Environment as Env
 import qualified System.Exit as Exit
+import System.FilePath ((</>))
 import qualified System.Information as Info
 import qualified System.IO as IO
 import qualified System.Process as Process
+import Text.RawString.QQ (r)
 
 
 
@@ -34,6 +38,9 @@ main =
       elm <- getElm
       root <- getRoot
 
+      npm <- getNpm
+      node <- getNode
+
       putStrLn "I am going to run a bunch of `elm` commands."
       putStrLn "I need `elm` to print to stdout to get realistic measurements."
       putStrLn "I will print out a nice summary after I am done!\n"
@@ -43,22 +50,24 @@ main =
       cpus <- Info.cpus
 
       warmup elm root
+      warmup elm root
       normal <- measure elm root []
       devnull <- measure elm root ["--output=/dev/null"]
+      sizes <- getSizes elm root npm node
 
-      let result = Results (show os) ram (Info.showCPUs cpus) normal devnull
+      let result = Results (show os) ram (Info.showCPUs cpus) normal devnull sizes
       Binary.encodeFile "results.dat" result
       render root result
 
 
-warmup :: Command -> FilePath -> IO ()
+warmup :: Elm -> FilePath -> IO ()
 warmup elm root =
   do  removeElmStuff
       _ <- _run elm ["make", root, "--output=/dev/null"]
       return ()
 
 
-measure :: Command -> FilePath -> [String] -> IO FlagResult
+measure :: Elm -> FilePath -> [String] -> IO FlagResult
 measure elm root flags =
   do  removeElmStuff
       scratch <- _run elm ("make" : root : flags)
@@ -67,7 +76,7 @@ measure elm root flags =
       return $ FlagResult scratch incrementals
 
 
-measureFile :: Command -> FilePath -> [String] -> FilePath -> IO FileResult
+measureFile :: Elm -> FilePath -> [String] -> FilePath -> IO FileResult
 measureFile elm root flags path =
   do  bytes <- fromIntegral <$> Dir.getFileSize path
       lines <- (+) 1 . BS.count 0x0A <$> BS.readFile path
@@ -102,6 +111,7 @@ data Results =
     , _cpus :: String
     , _normal :: FlagResult
     , _devnull :: FlagResult
+    , _sizes :: Sizes
     }
 
 
@@ -120,9 +130,17 @@ data FileResult =
     }
 
 
+data Sizes =
+  Sizes
+    { _initial :: Int
+    , _minified :: Int
+    , _gzipped :: Int
+    }
+
+
 instance Binary.Binary Results where
-  get = M.liftM5 Results get get get get get
-  put (Results a b c d e) = put a >> put b >> put c >> put d >> put e
+  get = Results <$> get <*> get <*> get <*> get <*> get <*> get
+  put (Results a b c d e f) = put a >> put b >> put c >> put d >> put e >> put f
 
 
 instance Binary.Binary FlagResult where
@@ -133,6 +151,11 @@ instance Binary.Binary FlagResult where
 instance Binary.Binary FileResult where
   get = M.liftM3 FileResult get get get
   put (FileResult a b c) = put a >> put b >> put c
+
+
+instance Binary.Binary Sizes where
+  get = M.liftM3 Sizes get get get
+  put (Sizes a b c) = put a >> put b >> put c
 
 
 
@@ -194,11 +217,11 @@ instance Json.FromJSON Version where
 -- GET ELM
 
 
-newtype Command =
-  Command { _run :: [String] -> IO Time }
+newtype Elm =
+  Elm { _run :: [String] -> IO Time }
 
 
-getElm :: IO Command
+getElm :: IO Elm
 getElm =
   do  maybeElm <- Dir.findExecutable "elm"
       elm <- check maybeElm "Could not find `elm` on your PATH."
@@ -208,7 +231,7 @@ getElm =
         then return ()
         else failure ("Found wrong version of `elm` on your PATH.\nFound " ++ vsn ++ " but need 0.19.1 for this data collection.")
 
-      return $ Command $ \args ->
+      return $ Elm $ \args ->
         do  let process = Process.proc elm args
             start <- Time.getSystemTime
             Process.withCreateProcess process $ \_ _ _ handle ->
@@ -259,17 +282,70 @@ getRam =
 
 
 
+-- GET NPM
+
+
+getNpm :: IO FilePath
+getNpm =
+  do  maybeNpm <- Dir.findExecutable "npm"
+      check maybeNpm "Could not find `npm` on your PATH.\nI need it to measure minification sizes."
+
+
+getNode :: IO FilePath
+getNode =
+  do  maybeNode1 <- Dir.findExecutable "node"
+      maybeNode2 <- Dir.findExecutable "nodejs"
+      check (maybeNode1 <|> maybeNode2) "Could not find `node` or `nodejs` on your PATH.\nI need it to measure minification sizes."
+
+
+
+-- GET SIZES
+
+
+getSizes :: Elm -> FilePath -> FilePath -> FilePath -> IO Sizes
+getSizes elm root npm node =
+  do  let dir = "elm-stuff" </> "0.19.1" </> "temporary"
+      let path = dir </> "elm.js"
+
+      _ <- _run elm ["make", root, "--output=" ++ path, "--optimize"]
+
+      putStrLn "Working on estimating asset sizes."
+
+      withinTempDir dir $
+        do  BS.writeFile "index.js" indexJS
+            BS.writeFile "package.json" packageJSON
+            BS.writeFile "package-lock.json" packageLockJSON
+            _ <- Process.readProcess npm ["install"] ""
+            o <- Process.readProcess node ["index.js"] ""
+            let [inital,minified,gzipped] = read o
+            return (Sizes inital minified gzipped)
+
+
+withinTempDir :: FilePath -> IO a -> IO a
+withinTempDir dir work =
+  E.bracket
+    (Dir.createDirectoryIfMissing True dir)
+    (\_ -> Dir.removeDirectoryRecursive dir)
+    (\_ -> Dir.withCurrentDirectory dir work)
+
+
+
 -- RENDER
 
 
 render :: FilePath -> Results -> IO ()
-render root (Results os ram cpus normal devnull@(FlagResult _ fs)) =
+render root (Results os ram cpus normal devnull@(FlagResult _ fs) (Sizes initial minified gzipped)) =
   do  putStrLn "\n-- OVERVIEW -----------------------------------------------\n"
       putStrLn $ "OS:  " ++ os
       putStrLn $ "RAM: " ++ show ram ++ "GB"
       putStrLn $ "CPU: " ++ cpus
 
       putStrLn $ "PROJECT: " ++ show (length fs) ++ " files, " ++ show (sum (map _lines fs)) ++ " lines\n"
+
+      putStrLn $ "ASSET SIZE: "
+        ++ show initial  ++ " bytes -> "
+        ++ show minified ++ " bytes (minified) -> "
+        ++ show gzipped  ++ " bytes (gzipped)\n"
 
       renderResults normal ["elm","make",root]
       renderResults devnull ["elm","make",root,"--output=/dev/null"]
@@ -328,3 +404,90 @@ check maybe message =
   case maybe of
     Nothing -> failure message
     Just a  -> return a
+
+
+
+indexJS :: BS.ByteString
+indexJS = [r|
+
+var fs = require('fs');
+var UglifyJS = require("uglify-js");
+var zlib = require('zlib');
+
+
+// MINIFY
+
+var phase1 = UglifyJS.minify(fs.readFileSync("elm.js", "utf8"), {
+  compress: {
+    pure_funcs: ["F2","F3","F4","F5","F6","F7","F8","F9","A2","A3","A4","A5","A6","A7","A8","A9"],
+    pure_getters: true,
+    keep_fargs: false,
+    unsafe_comps: true,
+    unsafe: true
+  },
+  mangle: false
+});
+
+var phase2 = UglifyJS.minify(phase1.code, {
+  compress: false,
+  mangle: true
+});
+
+
+// OUTPUT
+
+var initialSize = fs.statSync("elm.js").size;
+var minifiedSize = Buffer.from(phase2.code, 'utf8').length;
+var gzippedSize = zlib.gzipSync(phase2.code).length;
+
+console.log([initialSize, minifiedSize, gzippedSize]);
+
+|]
+
+
+packageJSON :: BS.ByteString
+packageJSON = [r|
+{
+  "name": "minify",
+  "version": "1.0.0",
+  "repository": "https://github.com/AUTHOR/PROJECT",
+  "description": "estimate asset sizes",
+  "main": "index.js",
+  "license": "BSD-3-Clause",
+  "dependencies": {
+    "uglify-js": "3.6.7"
+  }
+}
+|]
+
+
+packageLockJSON :: BS.ByteString
+packageLockJSON = [r|
+{
+  "name": "minify",
+  "version": "1.0.0",
+  "lockfileVersion": 1,
+  "requires": true,
+  "dependencies": {
+    "commander": {
+      "version": "2.20.3",
+      "resolved": "https://registry.npmjs.org/commander/-/commander-2.20.3.tgz",
+      "integrity": "sha512-GpVkmM8vF2vQUkj2LvZmD35JxeJOLCwJ9cUkugyk2nuhbv3+mJvpLYYt+0+USMxE+oj+ey/lJEnhZw75x/OMcQ=="
+    },
+    "source-map": {
+      "version": "0.6.1",
+      "resolved": "https://registry.npmjs.org/source-map/-/source-map-0.6.1.tgz",
+      "integrity": "sha512-UjgapumWlbMhkBgzT7Ykc5YXUT46F0iKu8SGXq0bcwP5dz/h0Plj6enJqjz1Zbq2l5WaqYnrVbwWOWMyF3F47g=="
+    },
+    "uglify-js": {
+      "version": "3.6.7",
+      "resolved": "https://registry.npmjs.org/uglify-js/-/uglify-js-3.6.7.tgz",
+      "integrity": "sha512-4sXQDzmdnoXiO+xvmTzQsfIiwrjUCSA95rSP4SEd8tDb51W2TiDOlL76Hl+Kw0Ie42PSItCW8/t6pBNCF2R48A==",
+      "requires": {
+        "commander": "~2.20.3",
+        "source-map": "~0.6.1"
+      }
+    }
+  }
+}
+|]
